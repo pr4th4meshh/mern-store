b215e1501ee3623ef27f99cb9afa0cf79db7d9e3
pr4th4meshh <prathameshasolkar17@gmail.com>
2024-07-06 13:54:53 +0530

    Add: remaining pages

---
+++ src/app/(productRoutes)/special/page.tsx
@@ -0,0 +1,42 @@
+"use client"
+import React from "react"
+import Image from "next/image"
+import HeroImage from "../../../../public/reinwedding.webp"
+import { useGetAllProductsQuery } from "@/lib/api-slices/productsApiSlice"
+import CardComponent from "@/components/ui/CardComponent"
+
+export const dynamic = "force-dynamic"
+
+const Special = () => {
+  const { data: productsData, isLoading: productsDataLoading } =
+    useGetAllProductsQuery(undefined)
+  return (
+    <div className="h-auto container mx-auto p-10 flex flex-col">
+      <Image
+        src={HeroImage}
+        alt="Special wear"
+        width="0"
+        height="0"
+        sizes="100vw"
+        className="h-[35vh] w-[90vw] rounded-2xl object-cover"
+        quality={100}
+        priority
+        placeholder="blur"
+      />
+      <h1 className="uppercase text-4xl text-center my-10">
+        check out some special wear:
+      </h1>
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+        {productsData?.map((product) => (
+          <CardComponent
+            key={product._id}
+            product={product}
+            isLoading={productsDataLoading}
+          />
+        ))}
+      </div>
+    </div>
+  )
+}
+
+export default Special
