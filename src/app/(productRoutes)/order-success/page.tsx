66827ef6a8c5ac5fba6abacfbec6838e3f4068e6
pr4th4meshh <prathameshasolkar17@gmail.com>
2024-07-21 15:38:58 +0530

    add: order success page

---
+++ src/app/(productRoutes)/order-success/page.tsx
@@ -0,0 +1,36 @@
+"use client"
+import React from "react"
+import { useRouter } from "next/navigation"
+import ButtonComponent from "@/components/ui/ButtonComponent"
+
+const OrderSuccessPage = () => {
+  const router = useRouter()
+
+  const handleGoBack = () => {
+    router.push("/")
+  }
+
+  return (
+    <div
+      className="flex flex-col items-center justify-center bg-gray-100"
+      style={{ height: "calc(100vh - 6rem)" }}
+    >
+      <div className="bg-white p-10 rounded-lg shadow-lg text-center">
+        <h1 className="text-3xl font-bold text-secondary mb-4 animate-fade-in">
+          Order Placed Successfully!
+        </h1>
+        <p className="text-lg text-gray-700 mb-2 animate-fade-in animation-delay-1">
+          Thank you for your purchase. Your order has been placed successfully.
+        </p>
+        <ButtonComponent
+          onClick={handleGoBack}
+          title="Go back Home"
+          textColor="text-white"
+          bgColor="bg-secondary"
+        />
+      </div>
+    </div>
+  )
+}
+
+export default OrderSuccessPage
